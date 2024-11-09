3658b6587a7b0457f805cf1367c60e782005fb1d
= <thivyesh.sivasuriyamoorthy@student.manchester.ac.uk>
2024-11-09 14:23:27 +0000

    start layout done

---
+++ website/script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    document.getElementById("Chronos").style.opacity = 1;
+    document.getElementById("slogan").style.opacity = 1;
+    document.getElementById("explore-button").style.opacity = 1;
+}
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+
